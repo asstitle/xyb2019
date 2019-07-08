168c0f628098eed19b20e6ed6a7c231ca404b24a
dkd <794863612@qq.com>
2019-07-08 13:48:40 +0800

    修改

--- app/admin/controller/ScheduleController.php
+++ app/admin/controller/ScheduleController.php
@@ -4,11 +4,15 @@ namespace app\admin\controller;
 
 
 use cmf\controller\AdminBaseController;
-
+//我的日程
 class ScheduleController extends AdminBaseController
 {
-    //我的日程
+    //我的课表
     public function index(){
+        return $this->fetch();
+    }
+    //我的调课情况
+    public function adjustment(){
         return $this->fetch();
     }
 }
