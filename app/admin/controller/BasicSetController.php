2b5421fcde16140b8ca006ec32b1288b0f5f2455
dkd <794863612@qq.com>
2019-07-08 16:08:20 +0800

    新增

---
+++ app/admin/controller/BasicSetController.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\admin\controller;
+use cmf\controller\AdminBaseController;
+//基本设置
+class BasicSetController extends AdminBaseController
+{
+     //班级信息
+     public function class_info(){
+         return $this->fetch();
+     }
+     //学年学期
+     public function school_semester(){
+         return $this->fetch();
+     }
+     //作息时间
+     public function time_table(){
+         return $this->fetch();
+     }
+     //课程信息
+     public function course_info(){
+         return $this->fetch();
+     }
+    //任教信息
+    public function teach_info(){
+         return $this->fetch();
+    }
+}
